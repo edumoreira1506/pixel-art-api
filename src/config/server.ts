5b95e0a69db0acc5a17a7647cb16d23488a3220f
Eduardo Moreira <contato@eduardoem.com.br>
2021-04-17 15:08:15 -0300

    Setup server

---
+++ src/config/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const App = express();
+
+App.use(
+  cors({
+	  origin: '*'
+  })
+);
+App.use(bodyParser.urlencoded({ extended: true }));
+App.use(bodyParser.json({ limit: '2048kb' }));
+
+App.use((_, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+	next();
+});
+
+export default App;
