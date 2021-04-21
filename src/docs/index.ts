145b7e595dcfef3c50e4cf2d320d893deb6ae9cd
Eduardo Moreira <contato@eduardoem.com.br>
2021-04-21 19:38:08 -0300

    Add art docs

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -2,6 +2,7 @@ import dotEnv from 'dotenv'
 
 import user from './user'
 import folder from './folder'
+import art from './art'
 
 dotEnv.config()
 
@@ -18,5 +19,6 @@ export default {
   paths: {
     ...user,
     ...folder,
+    ...art,
   }
 }
