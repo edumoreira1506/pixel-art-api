c4bde0f8289b9d34cc55dc30eb5fe1f41fe3aa62
Eduardo Moreira <contato@eduardoem.com.br>
2021-04-21 22:37:56 -0300

    Fix local env

--- ormconfig.ts
+++
@@ -1,6 +0,0 @@
-const isProduction = process.env.PRODUCTION === 'true'
-const databaseConfigPath = isProduction ? './build/config/database' : './src/config/database'
-
-const { databaseConfig } = require(databaseConfigPath)
-
-module.exports = databaseConfig
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,27 +1,3 @@
 import { createConnection } from 'typeorm'
 
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USERNAME } from '@Constants/database'
-
-const isProduction = process.env.PRODUCTION === 'true'
-
-export const databaseConfig = {
-  type: 'postgres',
-  host: DB_HOST,
-  port: 5432,
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  logging: true,
-  entities: [
-    isProduction ? 'build/entities/*.js' : 'src/entities/*.ts'
-  ],
-  migrations: [
-    isProduction ? 'build/database/migrations/**/*.js' : 'src/database/migrations/**/*.ts'
-  ],
-  subscribers: [],
-  cli: {
-    migrationsDir: isProduction ? 'build/database/migrations' : 'src/database/migrations'
-  }
-}
-
 createConnection().then(() => console.log('Connected to the database'))
