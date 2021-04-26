3f45345de4d54b14624aa56ec2a8362f335cb12e
Eduardo Moreira <contato@eduardoem.com.br>
2021-04-25 22:08:03 -0300

    Add route to show art (#11)

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -21,6 +21,7 @@ routes.patch('/folders/:folderId', withAuth, withFolderParam, FolderController.u
 
 routes.post('/folders/:folderId/arts', withAuth, withFolderParam, ArtController.store)
 routes.get('/folders/:folderId/arts', withAuth, withFolderParam, ArtController.index)
+routes.get('/folders/:folderId/arts/:artId', withAuth, withFolderParam, withArtParam, ArtController.show)
 routes.patch('/folders/:folderId/arts/:artId', withAuth, withFolderParam, withArtParam, ArtController.update)
 routes.delete('/folders/:folderId/arts/:artId', withAuth, withFolderParam, withArtParam, ArtController.remove)
 
